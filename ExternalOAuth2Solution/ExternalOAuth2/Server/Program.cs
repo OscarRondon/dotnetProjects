e84993d1f27209225dbbdfa25dff8fd2f3b8c6a1
orondon <oscar.rondon.c@gmail.com>
2023-09-04 19:58:19 -0300

    ExternalOAuth2 [Create DB, Create Google Key, Add Goggle sigin functionality]

--- ExternalOAuth2Solution/ExternalOAuth2/Server/Program.cs
+++ ExternalOAuth2Solution/ExternalOAuth2/Server/Program.cs
@@ -3,23 +3,44 @@ using ExternalOAuth2.Server.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Custon configuration File
+builder.Configuration.AddJsonFile("env.json", optional: true, reloadOnChange: true);
+
+// Get values from .env file
+string dbServer = builder.Configuration.GetSection("DataBase:DBServer").Value ?? "";
+string dbName = builder.Configuration.GetSection("DataBase:DBName").Value ?? "";
+string dbUser = builder.Configuration.GetSection("DataBase:DBUser").Value ?? "";
+string dbPwd = builder.Configuration.GetSection("DataBase:DBPwd").Value ?? "";
+string dbConnectioString = builder.Configuration.GetSection("DataBase:ConnectionStrings:MSSql").Value ?? "";
+dbConnectioString = dbConnectioString.Replace("{DBServer}", dbServer);
+dbConnectioString = dbConnectioString.Replace("{DBName}", dbName);
+dbConnectioString = dbConnectioString.Replace("{DBUser}", dbUser);
+dbConnectioString = dbConnectioString.Replace("{DBPwd}", dbPwd);
+
+var connectionString = dbConnectioString ?? throw new InvalidOperationException("Connection string not found.");
+
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddIdentityServer()
     .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
 
 builder.Services.AddAuthentication()
-    .AddIdentityServerJwt();
+    .AddIdentityServerJwt()
+    .AddGoogle(googleOptions =>
+    {
+        googleOptions.ClientId = builder.Configuration.GetSection("GoogleAuth:ClientId").Value;
+        googleOptions.ClientSecret = builder.Configuration.GetSection("GoogleAuth:ClientSecret").Value;
+    });
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
--- ExternalOAuth2Solution/README.md
+++ ExternalOAuth2Solution/README.md
@@ -1,4 +1,4 @@
-﻿#Authentication System [1 h]
+﻿#Authentication System [3 h]
 
 	- Project Template: Blazor WebAssembly
 		- Authentication type: Individual Accouny
@@ -15,22 +15,20 @@
 	- Microsoft.EntityFrameworkCore.SqlServer
 	- Microsoft.EntityFrameworkCore.Tools
 	- Microsoft.VisualStudio.Web.CodeGeneration.Design
-	- Microsoft.VisualStudio.Web.CodeGeneration.Utils	
+	- Microsoft.VisualStudio.Web.CodeGeneration.Utils
+	- Microsoft.AspNetCore.Authentication.Google
 
 ##Instructions
 
-	- Create a new project [ASP.Net Web App (MVC)]
-	- Right click on project > Add > New Scaffolded Item > Identity (choose the options of the presented form)
-	- Add config files
-	- Program.cs > Add Service for Razor Pages [builder.Services.AddRazorPages();]
-	- Program.cs > Map Razor Pages [app.MapRazorPages();]
+	- Create Google API Key (As indicates on documentation https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/google-logins?view=aspnetcore-7.0)
+	- On Program.cs Modyfy: builder.Services.AddAuthentication().AddIdentityServerJwt(); to builder.Services.AddAuthentication().AddIdentityServerJwt().AddGoogle(<you have to put the options>);
 	- Modify Auth model
 	- Run in Nuget Manager Console: Add-Migration "Initial Create"
 	- Run in Nuget Manager Console: Update-Database (for data base creation)
 
 ##Configuration
 
-	- Create a file "env.json" into root forlder, using "env.template.json"
+	- Create a file "env.json" into root forlder, using "env.template.json" (For Server)
 	- Set the "env.json" properties as:
 		- Build action: Content
 		- Copy to Output directory: Copy always
