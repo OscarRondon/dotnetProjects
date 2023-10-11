a9f38ab46a153ec566fa597baa993e186cac1fca
orondon <oscar.rondon.c@gmail.com>
2023-10-11 18:00:49 -0300

    eCommerceTickets [Add ViewComponent for ShoppingCart]

---
+++ eCommerceTicketsSolution/eCommerceTickets/Controllers/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,21 @@
+﻿using eCommerceTickets.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceTickets.Controllers.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCartService _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCartService shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await _shoppingCart.GetShoppingCartItems();
+            return View(items.Count);
+        }
+    }
+}
