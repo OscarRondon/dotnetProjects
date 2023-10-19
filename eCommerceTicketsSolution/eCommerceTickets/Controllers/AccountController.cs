e0a787e3c2d53e7446cb6f88a7b617effb22901b
orondon <oscar.rondon.c@gmail.com>
2023-10-19 16:48:21 -0300

    eCommerceTickets [Add User LoginVM,  User Login View, Login User functionality, Add User RegisterVM,  User Register View, Register User functionality]

--- eCommerceTicketsSolution/eCommerceTickets/Controllers/AccountController.cs
+++ eCommerceTicketsSolution/eCommerceTickets/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
 ﻿using eCommerceTickets.Data;
 using eCommerceTickets.Models;
+using eCommerceTickets.Models.Enums;
+using eCommerceTickets.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace eCommerceTickets.Controllers
 {
@@ -24,7 +27,87 @@ namespace eCommerceTickets.Controllers
             _signInManager = signInManager;
             _roleManager = roleManager;
         }
-        public IActionResult Index()
+
+        #region Login
+        //GET
+        public IActionResult Login()
+        {
+            return View(new LoginVM());
+        }
+
+        //POST
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.EmailAddress);
+
+            if (user != null)
+            {
+                var passWordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passWordCheck)
+                {
+                    var sigin = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (sigin.Succeeded)
+                        return RedirectToAction("Index", "Movies");
+                    else
+                    {
+                        TempData["Error"] = "Error Signing In";
+                        return View(loginVM);
+                    }
+                }
+                TempData["Error"] = "Invalid Password";
+                return View(loginVM);
+            }
+
+            TempData["Error"] = "Invalid User";
+            return View(loginVM);
+        }
+        #endregion
+
+        #region Register
+        //GET
+        public IActionResult Register()
+        {
+            return View(new RegisterVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User.ToString());
+
+            return View("RegisterCompleted");
+        }
+        #endregion
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Movies");
+        }
+
+        public IActionResult AccessDenied(string ReturnUrl)
         {
             return View();
         }
---
+++ eCommerceTicketsSolution/eCommerceTickets/ViewModels/LoginVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceTickets.ViewModels
+{
+    public class LoginVM
+    {
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
