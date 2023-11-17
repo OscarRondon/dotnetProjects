a5da6b9379481becb8ec4a7ebc2a025026cfe55a
orondon <oscar.rondon.c@gmail.com>
2023-11-17 18:35:41 -0300

    BlazorAppSolutions [ Page component, component, components propertie [Parameters], components root parameters]

---
+++ BlazorAppsSolutions/BlazorServerApp/BaseComponents/DateBaseComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorServerApp.BaseComponents
+{
+    public class DateBaseComponent: ComponentBase
+    {
+        public DateTime currentDate { get; set; } = DateTime.Now;
+
+        public DateBaseComponent()
+        {
+            
+        }
+    }
+}
