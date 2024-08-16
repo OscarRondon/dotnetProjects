3e46ed20d4a6704740e9c87809c5df6b1e197b0f
orondon <oscar.rondon.c@gmail.com>
2024-08-16 13:56:56 -0400

    PortafolioSolution [  * Working on email sending ]

--- PortafolioSolution/PortfolioShared/SendMail.cs
+++ PortafolioSolution/PortfolioShared/SendMail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,14 @@ namespace PortfolioShared
 {
     public class SendMail
     {
+        [Required]
         public string SenderName { get; set; }
+        [Required]
+        [EmailAddress]
         public string SenderEmail { get; set; }
+        [Required]
         public string Subject { get; set; }
+        [Required]
         public string Message { get; set; }
     }
 }
